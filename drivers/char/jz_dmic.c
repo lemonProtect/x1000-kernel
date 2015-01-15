4d8a690a7289ee2e44b6e1cc259b9633005b23ae
pzqi <pzqi_bj@ingenic.cn>
2015-01-15 16:40:01 +0800

    1.fix set dmic samplerate error.

--- drivers/char/jz_dmic.c
+++ drivers/char/jz_dmic.c
@@ -241,9 +241,8 @@ static long jzdmic_ioctl(struct file *filp, unsigned int cmd, unsigned long args
 			printk("not support , DMIC_SET_CHANNEL\n");
 			break;
 		case DMIC_SET_SAMPLERATE:
-			printk("dmic set samplerate\n");
 			copy_from_user(&samplerate, argp, sizeof(long));
-			ret = wakeup_module_ioctl(cmd, samplerate);
+			ret = wakeup_module_ioctl(DMIC_IOCTL_SET_SAMPLERATE, samplerate);
 			break;
 
 		case DMIC_GET_ROUTE:
--- tools/dmic-test/dmic-test.c
+++ tools/dmic-test/dmic-test.c
@@ -409,6 +409,25 @@ int main(int argc, char *argv[])
 	if (record_enable) {
 		printf("Record\n");
 		dev_fd = open_device(dmic_dev, O_RDONLY, &is_dev_reg_file);
+
+
+		/*SET SAMPLE RATE:*/
+
+#define DMIC_SET_CHANNEL    0x100
+#define DMIC_ENABLE         0x101
+#define DMIC_DISABLE        0x102
+#define DMIC_SET_SAMPLERATE 0x103
+#define DMIC_GET_ROUTE      0x104
+#define DMIC_SET_ROUTE      0x105
+#define DMIC_SET_DEVICE     0x106
+
+
+		int ret = 0;
+		ret = ioctl(dev_fd, DMIC_SET_SAMPLERATE, &speed);
+		if(ret < 0) {
+			printf("[ioctl] dmic set sample rate!\n");
+		}
+
 	}
 
 	if ( (record_enable) && (dev_fd < 0)) {
