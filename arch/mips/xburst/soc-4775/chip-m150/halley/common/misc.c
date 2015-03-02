5dbe1990e123f235e263498a0198fbac501e9fae
sccheng <sccheng_bj@ingenic.cn>
2015-03-02 22:48:26 +0800

    add wifi leds and bluetooth for halley

--- arch/mips/xburst/soc-4775/chip-m150/halley/common/misc.c
+++ arch/mips/xburst/soc-4775/chip-m150/halley/common/misc.c
@@ -3,6 +3,7 @@
 #include <linux/i2c-gpio.h>
 #include <linux/gpio_keys.h>
 #include <linux/input.h>
+#include <linux/leds.h>
 //#include <linux/tsc.h>
 #include <linux/spi/spi.h>
 #include <linux/spi/spi_gpio.h>
@@ -23,5 +24,41 @@
 struct jz_efuse_platform_data jz_efuse_pdata = {
 	    /* supply 2.5V to VDDQ */
 	    .gpio_vddq_en_n = GPIO_EFUSE_VDDQ,
+};
+#endif
+
+
+#ifdef CONFIG_LEDS_GPIO
+struct gpio_led jz_leds[] = {
+	[0]={
+		.name = "wl_led_r",
+		.gpio = WL_LED_R,
+		.active_low = 0,
+	},
+	[1]={
+		.name = "wl_led_g",
+		.gpio = WL_LED_G,
+		.active_low = 0,
+	},
+
+	[2]={
+		.name = "wl_led_b",
+		.gpio = WL_LED_B,
+		.active_low = 0,
+	},
+
+};
+
+struct gpio_led_platform_data  jz_led_pdata = {
+	.num_leds = 3,
+	.leds = jz_leds,
+};
+
+struct platform_device jz_led_rgb = {
+	.name       = "leds-gpio",
+	.id     = -1,
+	.dev        = {
+		.platform_data  = &jz_led_pdata,
+	}
 };
 #endif
