6effd6f6e6d2abfd7df50e4c06fad2f42a28a664
jwsun <jwsun@ingenic.cn>
2015-04-24 15:32:52 +0800

    1, rebuilt TCU driver 2, modify PWM driver 3, fix a irq bug about GPIO
    Signed-off-by: Sun Jiwei<jiwei.sun@ingenic.com>
    Test-by:Sun Jiwei<jiwei.sun@ingenic.com>

--- drivers/pwm/pwm-jz.c
+++ drivers/pwm/pwm-jz.c
@@ -1,32 +1,45 @@
-/*
- *  Copyright (C) 2014,  King liuyang <liuyang@ingenic.cn>
- *  jz_PWM support
+/* drivers/pwm/pwm-jz.c
+ * PWM driver of Ingenic's SoC M200
  *
+ * Copyright (C) 2015 Ingenic Semiconductor Co., Ltd.
+ *	http://www.ingenic.com
+ * Author:	King liuyang <liuyang@ingenic.cn>
+ * Modified:	Sun Jiwei <jiwei.sun@ingenic.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
-#include <linux/clk.h>
-#include <linux/err.h>
-#include <linux/gpio.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/err.h>
+#include <linux/slab.h>
+#include <linux/gpio.h>
 #include <linux/pwm.h>
-#include <mach/jztcu.h>
 #include <soc/extal.h>
 #include <soc/gpio.h>
-#include <linux/slab.h>
+#include <mach/jztcu.h>
 
-#define NUM_PWM 8
+#define NUM_PWM		(NR_TCU_CH)
 
 struct jz_pwm_device{
-	short id;
-	const char *label;
 	struct tcu_device *tcu_cha;
 };
 
 struct jz_pwm_chip {
-	struct clk *clk;
 	struct jz_pwm_device pwm_chrs[NUM_PWM];
 	struct pwm_chip chip;
 };
@@ -38,81 +51,59 @@ static inline struct jz_pwm_chip *to_jz(struct pwm_chip *chip)
 
 static int jz_pwm_request(struct pwm_chip *chip, struct pwm_device *pwm)
 {
-	struct jz_pwm_chip *jz = to_jz(chip);
+	struct jz_pwm_chip *jz_pwm = to_jz(chip);
 	struct tcu_device *tcu_pwm;
 	int id = pwm -> hwpwm;
-	if (id < 0 || id > 7)
-		return -ENODEV;
-
-	tcu_pwm = jz->pwm_chrs[id].tcu_cha;
-
-	tcu_pwm= tcu_request(id, NULL);
-
-	if(IS_ERR(tcu_pwm)) {
-		printk("-+-+-+-pwm_tcu_request failed!!-+-+-+\n");
+	if ((id < 0) || (id > 7) || (id == 5)) {
+		pr_err("%s, tcu channel %d is not exit\n", __func__, id);
 		return -ENODEV;
 	}
 
-	jz->pwm_chrs[id].id = id;
-	jz->pwm_chrs[id].label = pwm->label;
+	tcu_pwm= tcu_request(id);
+
+	if(IS_ERR(tcu_pwm)) {
+		pr_err("%s request tcu channel %d failed!\n", __func__, id);
+		return -ENODEV;
+	}
+
+	jz_pwm->pwm_chrs[id].tcu_cha = tcu_pwm;
 
 	tcu_pwm->pwm_flag = pwm->flags;
-	tcu_pwm->irq_type = NULL_IRQ_MODE;
 	tcu_pwm->init_level = 0;
-	printk("request pwm channel %d successfully\n", id);
+	pr_debug("%s, request pwm channel %d successfully\n", __func__, id);
 
 	return 0;
 }
 
 static void jz_pwm_free(struct pwm_chip *chip, struct pwm_device *pwm)
 {
-	struct jz_pwm_chip *jz = to_jz(chip);
-	struct tcu_device *tcu_pwm = jz->pwm_chrs[pwm->hwpwm].tcu_cha;
+	struct jz_pwm_chip *jz_pwm = to_jz(chip);
+	struct tcu_device *tcu_pwm = jz_pwm->pwm_chrs[pwm->hwpwm].tcu_cha;
 	tcu_free(tcu_pwm);
-	kfree (tcu_pwm);
-	kfree (jz);
-}
-
-static int jz_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
-{
-	struct jz_pwm_chip *jz = to_jz(chip);
-	struct tcu_device *tcu_pwm = jz->pwm_chrs[pwm->hwpwm].tcu_cha;
-
-	tcu_enable(tcu_pwm);
-
-	jzgpio_set_func(GPIO_PORT_E, GPIO_FUNC_0, 0x1 << pwm->hwpwm);
-
-	return 0;
-}
-
-static void jz_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm)
-{
-	struct jz_pwm_chip *jz = to_jz(chip);
-	struct tcu_device *tcu_pwm = jz->pwm_chrs[pwm->hwpwm].tcu_cha;
-
-	tcu_disable(tcu_pwm);
-
-	jzgpio_set_func(GPIO_PORT_E, GPIO_OUTPUT0, 0x1 << pwm->hwpwm);
 }
 
 static int jz_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 			     int duty_ns, int period_ns)
 {
+	struct jz_pwm_chip *jz_pwm = to_jz(chip);
+	int id = pwm->hwpwm;
+	struct tcu_device *tcu_pwm = jz_pwm->pwm_chrs[id].tcu_cha;
 	unsigned long long tmp;
 	unsigned long period, duty;
-	struct jz_pwm_chip *jz = to_jz(chip);
 	int prescaler = 0; /*prescale = 0,1,2,3,4,5*/
 
-	int id = pwm->hwpwm;
-	struct tcu_device *tcu_pwm = jz->pwm_chrs[id].tcu_cha;
-
-	if (duty_ns < 0 || duty_ns > period_ns)
+	if (duty_ns < 0 || duty_ns > period_ns) {
+		pr_err("%s, duty_ns(%d)< 0 or duty_ns > period_ns(%d)\n",
+				__func__, duty_ns, period_ns);
 		return -EINVAL;
-	if (period_ns < 200 || period_ns > 1000000000)
+	}
+	if (period_ns < 200 || period_ns > 1000000000) {
+		pr_err("%s, period_ns(%d) > 100000000 or < 200\n",
+				__func__, period_ns);
 		return -EINVAL;
+	}
 
 	tcu_pwm->id = id;
-	tcu_pwm->irq_type = NULL_IRQ_MODE;
 	tcu_pwm->pwm_flag = pwm->flags;
 
 #ifndef CONFIG_SLCD_SUSPEND_ALARM_WAKEUP_REFRESH
@@ -129,6 +120,7 @@ static int jz_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 		period >>= 2;
 		++prescaler;
 	}
+
 	if (prescaler == 6)
 		return -EINVAL;
 
@@ -155,6 +147,24 @@ static int jz_pwm_config(struct pwm_chip *chip, struct pwm_device *pwm,
 	return 0;
 }
 
+static int jz_pwm_enable(struct pwm_chip *chip, struct pwm_device *pwm)
+{
+	struct jz_pwm_chip *jz_pwm = to_jz(chip);
+	struct tcu_device *tcu_pwm = jz_pwm->pwm_chrs[pwm->hwpwm].tcu_cha;
+
+	tcu_enable(tcu_pwm);
+
+	return 0;
+}
+
+static void jz_pwm_disable(struct pwm_chip *chip, struct pwm_device *pwm)
+{
+	struct jz_pwm_chip *jz_pwm = to_jz(chip);
+	struct tcu_device *tcu_pwm = jz_pwm->pwm_chrs[pwm->hwpwm].tcu_cha;
+
+	tcu_disable(tcu_pwm);
+}
+
 static const struct pwm_ops jz_pwm_ops = {
 	.request = jz_pwm_request,
 	.free = jz_pwm_free,
@@ -166,50 +176,43 @@ static const struct pwm_ops jz_pwm_ops = {
 
 static int jz_pwm_probe(struct platform_device *pdev)
 {
-	struct jz_pwm_chip *jz;
-	struct tcu_device *tcu_pwm;
-	int ret, i;
+	struct jz_pwm_chip *jz_pwm;
+	int ret;
 
-	jz = devm_kzalloc(&pdev->dev, sizeof(*jz), GFP_KERNEL);
-	tcu_pwm = devm_kzalloc(&pdev->dev,(NUM_PWM * sizeof(*tcu_pwm)),GFP_KERNEL);
-
-	if (!jz || !tcu_pwm)
+	jz_pwm = devm_kzalloc(&pdev->dev,
+			sizeof(struct jz_pwm_chip), GFP_KERNEL);
+	if (!jz_pwm) {
+		pr_err("%s %d,malloc jz_pwm_chip error\n",
+				__func__, __LINE__);
 		return -ENOMEM;
-
-	jz->clk = clk_get(NULL, "tcu");
-	if (IS_ERR(jz->clk))
-		return PTR_ERR(jz->clk);
-
-	jz->chip.dev = &pdev->dev;
-	jz->chip.ops = &jz_pwm_ops;
-	jz->chip.npwm = NUM_PWM;
-	jz->chip.base = -1;
-
-	for(i=0; i < NUM_PWM; i++){
-		jz->pwm_chrs[i].tcu_cha = tcu_pwm++;
 	}
 
-	ret = pwmchip_add(&jz->chip);
+	jz_pwm->chip.dev = &pdev->dev;
+	jz_pwm->chip.ops = &jz_pwm_ops;
+	jz_pwm->chip.npwm = NUM_PWM;
+	jz_pwm->chip.base = -1;
+
+	ret = pwmchip_add(&jz_pwm->chip);
 	if (ret < 0) {
-		clk_put(jz->clk);
+		devm_kfree(&pdev->dev, jz_pwm);
 		return ret;
 	}
 
-	platform_set_drvdata(pdev, jz);
+	platform_set_drvdata(pdev, jz_pwm);
 
 	return 0;
 }
 
 static int jz_pwm_remove(struct platform_device *pdev)
 {
-	struct jz_pwm_chip *jz = platform_get_drvdata(pdev);
+	struct jz_pwm_chip *jz_pwm = platform_get_drvdata(pdev);
 	int ret;
 
-	ret = pwmchip_remove(&jz->chip);
+	ret = pwmchip_remove(&jz_pwm->chip);
 	if (ret < 0)
 		return ret;
 
-	clk_put(jz->clk);
+	devm_kfree(&pdev->dev, jz_pwm);
 
 	return 0;
 }
@@ -224,7 +227,6 @@ static struct platform_driver jz_pwm_driver = {
 };
 module_platform_driver(jz_pwm_driver);
 
-MODULE_AUTHOR("King liuyang <liuyang@ingenic.cn>");
-MODULE_DESCRIPTION("Ingenic jz PWM driver");
+MODULE_DESCRIPTION("Ingenic SoC PWM driver");
 MODULE_ALIAS("platform:jz-pwm");
 MODULE_LICENSE("GPL");
