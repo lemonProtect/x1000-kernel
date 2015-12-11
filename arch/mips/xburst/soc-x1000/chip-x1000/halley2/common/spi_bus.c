3a23fa856af5ae6ccda9cc93cccbb8593703a503
ZhangRonghua <ronghua.zhang@ingenic.com>
2015-12-11 11:58:12 +0800

    [network] add ipv4 configuration to halley2_nor_v10_linux_defconfig and support  32M spi nor on halley2 board

--- arch/mips/xburst/soc-x1000/chip-x1000/halley2/common/spi_bus.c
+++ arch/mips/xburst/soc-x1000/chip-x1000/halley2/common/spi_bus.c
@@ -203,6 +203,16 @@ struct spi_quad_mode  flash_quad_mode[] = {
 		.cmd_read = CMD_QUAD_READ,
 		.sfc_mode = TRAN_SPI_QUAD,
 	},
+	{
+		.RDSR_CMD = CMD_RDSR,
+		.WRSR_CMD = CMD_WRSR,
+		.RDSR_DATE = 0x40,//the data is write the spi status register for QE bit
+		.RD_DATE_SIZE = 1,
+		.WRSR_DATE = 0x40,//this bit should be the flash QUAD mode enable
+		.WD_DATE_SIZE = 1,
+		.cmd_read = CMD_QUAD_READ,
+		.sfc_mode = TRAN_SPI_QUAD,
+	},
 
 };
 #endif
@@ -332,7 +342,32 @@ struct spi_nor_platform_data spi_nor_pdata[] = {
 #ifdef CONFIG_SPI_QUAD
 			.quad_mode = &flash_quad_mode[2],
 #endif
-	}
+	},
+	{
+		.name           = "GD25Q256C",
+		.pagesize       = 256,
+		.sectorsize     = 4 * 1024,
+		.chipsize       = 32768 * 1024,
+		.erasesize      = 32 * 1024,//4 * 1024,
+		.id             = 0xc84019,
+
+		.block_info     = flash_block_info,
+		.num_block_info = ARRAY_SIZE(flash_block_info),
+
+		.addrsize       = 4,
+		.pp_maxbusy     = 3,            /* 3ms */
+		.se_maxbusy     = 400,          /* 400ms */
+		.ce_maxbusy     = 8 * 10000,    /* 80s */
+
+		.st_regnum      = 3,
+#if defined(CONFIG_MTD_JZ_SPI_NORFLASH) || defined(CONFIG_MTD_JZ_SFC_NORFLASH)
+		.mtd_partition  = jz_mtd_partition1,
+		.num_partition_info = ARRAY_SIZE(jz_mtd_partition1),
+#endif
+#ifdef CONFIG_SPI_QUAD
+		.quad_mode = &flash_quad_mode[3],
+#endif
+	},
 };
 #endif	/* defined(CONFIG_JZ_SPI_NOR) || defined(CONFIG_JZ_SFC_NOR) */
 
@@ -374,6 +409,7 @@ struct jz_spi_info spi0_info_cfg = {
 #endif
 
 #ifdef CONFIG_JZ_SFC
+#ifdef CONFIG_JZ_SFC_NOR
 struct jz_sfc_info sfc_info_cfg = {
 	.chnl = 0,
 	.bus_num = 0,
@@ -381,6 +417,14 @@ struct jz_sfc_info sfc_info_cfg = {
 	.board_info = spi_nor_pdata,
 	.board_info_size = ARRAY_SIZE(spi_nor_pdata),
 };
+#elif defined(CONFIG_JZ_SFCNAND)
+struct jz_sfc_info sfc_info_cfg = {
+	.chnl = 0,
+	.bus_num = 0,
+	.num_chipselect = 1,
+	.board_info = &jz_spi_nand_data,
+};
+#endif
 #endif	/* CONFIG_JZ_SFC */
 
 #ifdef CONFIG_SPI_GPIO
