26dba03d0aae5bdecdafdfa0bed237b01b8cc4d1
liu bo <bo.liu@ingenic.com>
2016-09-02 16:07:18 +0800

    add new sleep driver

--- arch/mips/xburst/soc-x1000/include/soc/tcsm_layout.h
+++ arch/mips/xburst/soc-x1000/include/soc/tcsm_layout.h
@@ -5,14 +5,14 @@
  *      |-------------|
  *      |    BANK0    |
  *      |-------------|     <--- SLEEP_TCSM_BOOTCODE_TEXT
- *      | BOOT CODE   |
- *      |-------------|     <--- SLEEP_TCSM_RESUMECODE_TEXT
- *      |    ...      |
- *      | RESUME CODE |
- *      |    ...      |
- *      |-------------|     <--- SLEEP_TCSM_RESUME_DATA
- *      | RESUME DATA |
- *      |-------------|     <--- SLEEP_TCSM_CPU_RESMUE_SP
+ *      |             |
+ *      |             |
+ *      |             |
+ *      |             |
+ *      |             |
+ *      |             |
+ *      |             |
+ *      |-------------|
  *      | VO DMA DESC |
  *      |_____________|     <--- VOICE_TCSM_DMA_DESC
  *
@@ -41,16 +41,8 @@
 #define TCSM_BANK_LEN              4096
 
 #define SLEEP_TCSM_BOOT_TEXT       (SLEEP_TCSM_SPACE)
-#define SLEEP_TCSM_BOOT_LEN        64
-#define SLEEP_TCSM_BOOT_END        (SLEEP_TCSM_BOOT_TEXT + SLEEP_TCSM_BOOT_LEN)
 
-#define SLEEP_TCSM_RESUME_TEXT     (SLEEP_TCSM_BOOT_END)
-#define SLEEP_TCSM_RESUME_LEN      2048
-#define SLEEP_TCSM_RESUME_END      (SLEEP_TCSM_RESUME_TEXT + SLEEP_TCSM_RESUME_LEN)
-
-#define SLEEP_TCSM_RESUME_DATA     (SLEEP_TCSM_RESUME_END)
-
-#define VOICE_TCSM_DMA_DESC_LEN    1024
+#define VOICE_TCSM_DMA_DESC_LEN    512
 #define SLEEP_TCSM_SPACE_END       (SLEEP_TCSM_SPACE + TCSM_BANK_LEN - VOICE_TCSM_DMA_DESC_LEN)
 #define VOICE_TCSM_DMA_DESC_ADDR   SLEEP_TCSM_SPACE_END
 
